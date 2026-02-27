754a696878958ee2752c34f498750bdffb38e018
Diego Lozoya Morales <diego.lozoya@gmail.com>
2026-02-27 15:16:10 -0600

    added xgboost histogram, importance plot & shap values barplot

--- main.py
+++ main.py
@@ -9,20 +9,11 @@ def main():
 
     # === Get Benchmark Results ===
     acc, auc, class_report = benchmark(df)
-    acc_xgb, auc_xgb, class_report_xgb, xgb_model = xgboost(df)
-
 
     print(f"\nAccuracy: {acc:.4f}")
     print(f"AUC Score: {auc:.4f}")
     print("\nClassification Report:")
     print(class_report)
-
-    print(f"\nXGBoost Accuracy: {acc_xgb:.4f}")
-    print(f"XGBoost AUC Score: {auc_xgb:.4f}")
-    print("\nXGBoost Classification Report:")
-    print(class_report_xgb)
-
-    plot_importance(xgb_model)
     
 if __name__ == "__main__":
     main()
